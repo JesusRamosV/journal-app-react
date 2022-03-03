26f8fa3b33e717891364b5cae6f7ec762052d98d
Jesus Ramos <dev.jesusramos@gmail.com>
2022-03-03 12:44:37 -0500

    Instalando y configurando Thunk Middleware - Acciones asincronas

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,17 @@
-import { types } from "../types/types"
+import { types } from "../types/types";
+
+export const loginstartEmailPassword = (email, password) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(login(123, "Pedro"));
+    }, 3500);
+  };
+};
 
 export const login = (uid, displayName) => ({
-        type: types.login,
-        payload: {
-            uid,
-            displayName
-        }
-
-})
+  type: types.login,
+  payload: {
+    uid,
+    displayName,
+  },
+});
--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { login } from "../../actions/auth";
+import { login, loginstartEmailPassword } from "../../actions/auth";
 import { useForm } from "../../custom-hooks/useForm";
 
 export const LoginScreen = () => {
@@ -18,7 +18,7 @@ export const LoginScreen = () => {
   const handleLogin = (e) => {
     e.preventDefault();
 
-    dispatch( login(1234, 'Jesus'));
+    dispatch( loginstartEmailPassword(email,password));
   }
 
   return (
